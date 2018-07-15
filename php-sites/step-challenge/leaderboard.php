c3e9f81f4b3de1830635499a7888b61cfc3681f9
Duncan Laidlaw <duncan.laidlaw@btinternet.com>
2018-07-15 21:45:01 +0100

    performance indicators added

--- php-sites/step-challenge/leaderboard.php
+++ php-sites/step-challenge/leaderboard.php
@@ -2,19 +2,31 @@
 <!--leaderboard table-->
     <table class="table table-hover">           
             <tr>
+                <th scope="col"></th>
                 <th scope="col">TEAM</th>
                 <th scope="col">STEPS</th>
             </tr>          
 <?php
-    $sql = "SELECT team, SUM(steps) AS totalSteps FROM  users, posts WHERE users.id = userid GROUP BY team ORDER BY SUM(steps) DESC";
+    $sql = "SELECT team, SUM(steps) AS totalSteps FROM  users, posts WHERE users.id = userid GROUP BY team ORDER BY SUM(steps) DESC LIMIT 3";
     $result = $db->query($sql);
         if ($result->num_rows > 0) {
+            $count = 1;
             // output data of each row
             while($row = $result->fetch_assoc()) {
+                if($count==1){
+                    $pos = '<div class="medal"><img src="images/gold.png" alt="gold" width="30px" height="30px"></div>';
+                }elseif($count==2){
+                    $pos = '<div class="medal"><img src="images/silver.png" alt="gold" width="30px" height="30px"></div>';
+                }elseif($count==3){
+                    $pos = '<div class="medal"><img src="images/bronze.png" alt="gold" width="30px" height="30px"></div>';
+                }else{
+                    $pos = '';
+                }
                 //echo "id: " . $row["id"]. " - Name: " . $row["username"]. " " . $row["email"]. "<br>";
                 echo "<tr>";
-                echo "<td>" . $row["team"] . "</td><td>" . $row["totalSteps"] . "</td>";
+                echo "<td>" . $pos ." </td><td>" . $row["team"] . "</td><td>" . $row["totalSteps"] . "</td>";
                 echo "</tr>";
+                $count++;
             }
         } else {
             echo "0 results";
--- php-sites/step-challenge/user_leaderboard.php
+++ php-sites/step-challenge/user_leaderboard.php
@@ -1,19 +1,31 @@
 <!--user leaderboard table-->
 <table class="table table-hover">           
     <tr>
+        <th scope="col"></th>
         <th scope="col">USER</th>
         <th scope="col">STEPS</th>
     </tr>          
 <?php
-    $sql = "SELECT username, SUM(steps) AS totalSteps FROM  users, posts WHERE users.id = userid GROUP BY username ORDER BY SUM(steps) DESC";
+    $sql = "SELECT username, SUM(steps) AS totalSteps FROM  users, posts WHERE users.id = userid GROUP BY username ORDER BY SUM(steps) DESC LIMIT 3";
     $result = $db->query($sql);
         if ($result->num_rows > 0) {
+            $count = 1;
+            
             // output data of each row
             while($row = $result->fetch_assoc()) {
+                if($count==1){
+                    $pos = '<div class="medal"><img src="images/gold.png" alt="gold" width="30px" height="30px"></div>';
+                }elseif($count==2){
+                    $pos = '<div class="medal"><img src="images/silver.png" alt="gold" width="30px" height="30px"></div>';
+                }elseif($count==3){
+                    $pos = '<div class="medal"><img src="images/bronze.png" alt="gold" width="30px" height="30px"></div>';
+                }
+                
                 //echo "id: " . $row["id"]. " - Name: " . $row["username"]. " " . $row["email"]. "<br>";
                 echo "<tr>";
-                echo "<td>" . $row["username"] . "</td><td>" . $row["totalSteps"] . "</td>";
+                echo "<td>" . $pos ." </td><td>" . $row["username"] . "</td><td>" . $row["totalSteps"] . "</td>";
                 echo "</tr>";
+                $count++;
             }
         } else {
             echo "0 results";
